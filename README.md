eee3ccd8e947263bece929ef0337c058c1721c13
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2022-05-05 00:49:13 +0200

    feat: add readme, fix lint errors

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Flow Wizard
+# flow-wizard
+
+Flow Wizard built using Typescript and React, uses LocalStorage as DB.
+
+### CI / CD
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/4f615b92-82ed-451d-8939-9f86d3f8c0f5/deploy-status)](https://app.netlify.com/sites/quizzical-cray-6ae2e4/deploys)
+![Node.js CI](https://github.com/AntonioErdeljac/js-pokedex/workflows/Node.js%20CI/badge.svg)
+
+[Live version](https://reverent-lewin-6110a6.netlify.app/)
+
+### Lint
+
+[![eslint: airbnb](https://img.shields.io/badge/Eslint-Airbnb-red?logo=airbnb&style=flat)](https://github.com/airbnb/javascript)
+
+### Test
+
+[![testing: Jest](https://img.shields.io/badge/Tests-Jest-red?logo=jest&style=flat)](https://github.com/facebook/jest)
+[![testing: Enzyme](https://img.shields.io/badge/Tests-Enzyme-red?logo=enzyme&style=flat)](https://github.com/enzymejs/enzyme)
+
+### Cloning the repository
+
+```shell
+git clone https://github.com/AntonioErdeljac/flow-wizard.git
+```
+
+### Install packages
+
+```shell
+npm i
+```
+
+## Available commands
+
+Running commands with npm `npm run [command]`
+
+| command | description                          |
+| :------ | :----------------------------------- |
+| `start` | Starts a development instance of app |
+| `build` | Creates a production bundle          |
+| `test`  | Opens the test suite                 |
--- src/components/IntentExpression/index.tsx
+++ src/components/IntentExpression/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {  Group, MultiSelect, Paper, Text, ThemeIcon } from '@mantine/core';
+import {
+  Group, MultiSelect, Paper, Text, ThemeIcon,
+} from '@mantine/core';
 import { PlayerPlay } from 'tabler-icons-react';
 
 type Props = {
@@ -9,21 +11,21 @@ type Props = {
   values: any;
 };
 
-const IntentExpression: React.FC<Props> = ({ description, expressions = [], onChange, values }) => {
-  return (
-    <Paper radius="md" withBorder p="lg">
-      <Group mb="xl">
-        <ThemeIcon variant="light" size={60} radius={60}>
-          <PlayerPlay size={30} />
-        </ThemeIcon>
-        <Group direction="column" spacing={0}>
-          <Text weight={500}>When...</Text>
-          <Text color="dimmed">{description}</Text>
-        </Group>
+const IntentExpression: React.FC<Props> = ({
+  description, expressions = [], onChange, values,
+}) => (
+  <Paper radius="md" withBorder p="lg">
+    <Group mb="xl">
+      <ThemeIcon variant="light" size={60} radius={60}>
+        <PlayerPlay size={30} />
+      </ThemeIcon>
+      <Group direction="column" spacing={0}>
+        <Text weight={500}>When...</Text>
+        <Text color="dimmed">{description}</Text>
       </Group>
-      <MultiSelect value={values} onChange={onChange} label="Expressions" data={expressions} />
-    </Paper>
-  );
-}
+    </Group>
+    <MultiSelect value={values} onChange={onChange} label="Expressions" data={expressions} />
+  </Paper>
+);
 
 export default IntentExpression;
